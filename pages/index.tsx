231fb7010d28a102769cfb95e0c51a8771549053
Christian Kang <christiankang@christians-air.lan>
2025-07-19 18:35:44 -0600

    chore: change heading color to test a PR

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center text-center px-4">
-      <h1 className="text-4xl font-bold text-blue-600">Christian Kang</h1>
+      <h1 className="text-4xl font-bold text-green-600">Christian Kang 👨‍💻</h1>
       <p className="mt-2 text-lg text-gray-700">Software Engineer · Backend + Full Stack</p>
       <div className="mt-4 space-x-4">
         <a href="#" className="text-blue-500 underline">GitHub</a>
